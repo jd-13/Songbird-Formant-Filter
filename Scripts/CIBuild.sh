04299d93685cb04ac5b7f79f63f8060a8ffebeb3
jd-13 <jackd8989@gmail.com>
2020-10-10 10:10:07 +0100

    Add CI build

---
+++ Scripts/CIBuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+
+# Download WE-Core
+WECORE_HOME="$(cd $SCRIPT_DIR/.. > /dev/null 2>&1 && pwd)"
+WECORE_SRC="$WECORE_HOME/WECore"
+git clone https://github.com/jd-13/WE-Core $SCRIPT_DIR/../WECore
+
+# We can't download the fonts so create blank files in their place
+touch $SCRIPT_DIR/../Source/Graphics/Montserrat-Regular.ttf
+
+# Set JUCE module paths
+sed -i 's#../JUCE/modules#/home/JUCE/modules#g' $SCRIPT_DIR/../Songbird.jucer
+
+# Generate the makefile
+$JUCE_PATH/Projucer --resave $SCRIPT_DIR/../Songbird.jucer
+
+# Update the include paths
+sed -i 's#-I$(HOME)/WEA/WECore/WECore#-I../../WECore/WECore#g' $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
+
+# Run the build
+cd $SCRIPT_DIR/../Builds/LinuxMakefile
+make
