b3fa74148a87e042643bf15111747ffd2c8f9c5d
jd-13 <jackd8989@gmail.com>
2020-12-20 21:53:07 +0000

    Additional CI logging

--- Scripts/CIBuild.sh
+++ Scripts/CIBuild.sh
@@ -5,23 +5,31 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 
 # Download WE-Core
-WECORE_HOME="$(cd $SCRIPT_DIR/.. > /dev/null 2>&1 && pwd)"
-WECORE_SRC="$WECORE_HOME/WECore"
-git clone https://github.com/jd-13/WE-Core $SCRIPT_DIR/../WECore
+echo "=== Cloning WE-Core ==="
+WECORE_HOME=$SCRIPT_DIR/../WECore
+git clone https://github.com/jd-13/WE-Core $WECORE_HOME
 
-# Download fonts from github
+cd $WECORE_HOME
+echo "=== Using WE-Core $(git log --pretty=format:'%h' -n 1) ==="
+cd -
+
+echo "=== Downloading fonts ==="
 wget https://github.com/JulietaUla/Montserrat/raw/master/fonts/ttf/Montserrat-Regular.ttf \
      -P $SCRIPT_DIR/../Source/Graphics
 
 # Set JUCE module paths
+echo "=== Setting module paths ==="
 sed -i 's#../JUCE/modules#/home/JUCE/modules#g' $SCRIPT_DIR/../Songbird.jucer
 
 # Generate the makefile
+echo "=== Generating makefile ==="
 $JUCE_PATH/Projucer --resave $SCRIPT_DIR/../Songbird.jucer
 
 # Update the include paths
+echo "=== Updating include paths ==="
 sed -i 's#-I$(HOME)/WEA/WECore/WECore#-I../../WECore/WECore#g' $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
 
 # Run the build
+echo "=== Starting build ==="
 cd $SCRIPT_DIR/../Builds/LinuxMakefile
 make
