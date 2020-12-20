867abc1a37af645081b3708cf6ed6271145d6983
jd-13 <jackd8989@gmail.com>
2020-12-20 22:22:06 +0000

    Remove redundant comments

--- Scripts/BuildLinux.sh
+++ Scripts/BuildLinux.sh
@@ -4,7 +4,6 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 
-# Download WE-Core
 echo "=== Cloning WE-Core ==="
 WECORE_HOME=$SCRIPT_DIR/../WECore
 git clone https://github.com/jd-13/WE-Core $WECORE_HOME
@@ -17,19 +16,15 @@ echo "=== Downloading fonts ==="
 wget https://github.com/JulietaUla/Montserrat/raw/master/fonts/ttf/Montserrat-Regular.ttf \
      -P $SCRIPT_DIR/../Source/Graphics
 
-# Set JUCE module paths
 echo "=== Setting module paths ==="
 sed -i 's#../JUCE/modules#/home/JUCE/modules#g' $SCRIPT_DIR/../Songbird.jucer
 
-# Generate the makefile
 echo "=== Generating makefile ==="
 $JUCE_PATH/Projucer --resave $SCRIPT_DIR/../Songbird.jucer
 
-# Update the include paths
 echo "=== Updating include paths ==="
 sed -i 's#-I$(HOME)/WEA/WECore/WECore#-I../../WECore/WECore#g' $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
 
-# Run the build
 echo "=== Starting build ==="
 cd $SCRIPT_DIR/../Builds/LinuxMakefile
 make
